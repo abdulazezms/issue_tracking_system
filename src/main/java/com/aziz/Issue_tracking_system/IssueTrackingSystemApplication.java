929c84dbf903d222b986b1f2dc6127939a64758f
Abdulazizms <abdulaziizmohammeds@gmail.com>
2022-11-15 06:57:09 +0300

    initial commit

---
+++ src/main/java/com/aziz/Issue_tracking_system/IssueTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.aziz.Issue_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aziz/Issue_tracking_system/IssueTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aziz.Issue_tracking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
