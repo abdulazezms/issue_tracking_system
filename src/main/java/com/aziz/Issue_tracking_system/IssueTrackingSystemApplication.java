1aa4b56b2dad126125e154dfe806cb0088e97240
Abdulazizms <abdulaziizmohammeds@gmail.com>
2022-11-17 17:22:59 +0300

    add SSL certificate

--- src/main/java/com/aziz/Issue_tracking_system/IssueTrackingSystemApplication.java
+++ src/main/java/com/aziz/Issue_tracking_system/IssueTrackingSystemApplication.java
@@ -1,13 +1,55 @@
 package com.aziz.Issue_tracking_system;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IssueTrackingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IssueTrackingSystemApplication.class, args);
+	}
+
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		// Enable SSL
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		// Add HTTP to HTTPS redirect
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+		return tomcat;
+	}
+
+	/*
+    We need to redirect from HTTP to HTTPS. Without SSL, this application uses port 8080, which is insecure (plaintext data transfer).
+    With SSL it will use port 8443. The below method redirects any request at port 8080 (HTTP) on our issue tracker app into port 8443 (HTTPS)
+     */
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
 	}
 
 }
